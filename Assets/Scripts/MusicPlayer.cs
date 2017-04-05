29bb2e03e9572339a212ce43decfbb30b7da16c6
Diogo Rodrigues <diogofgr@gmail.com>
2017-04-05 21:37:16 +0100

    start repo

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake() {
+		if (instance != null) {
+			// destroy new music player (keep previous):
+			Destroy (gameObject);
+		}
+		else {
+			// create music player:			
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
